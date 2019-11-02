9fdf4d0b76d923965de1843a7e5f1faf4d2af020
Luke Short <ekultails@gmail.com>
2019-11-02 17:30:05 -0400

    Only use yum to install packages once.
    Using it more than one time leads to a repository cache issue with the chroot.

--- chroot_create.sh
+++ chroot_create.sh
@@ -13,6 +13,5 @@ name=centos_8_appstream_aarch64
 baseurl=http://mirror.centos.org/centos/8/AppStream/aarch64/os/
 enabled=1
 EOF
-sudo yum groupinstall standard --installroot=/home/rock64/centos_8_aarch64
 sudo yum install centos-release dnf openssh-server sudo @base --installroot=/home/rock64/centos_8_aarch64
 sudo tar -c -f centos_8_aarch64.tar centos_8_aarch64
