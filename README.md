dcd2817bb1e27ec705fca8e84e7fc1c3d9db2859
Luke Short <ekultails@gmail.com>
2019-11-11 22:48:26 -0500

    Update rock64_fedora to build an image using a premade rootfs.

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 
 Build scripts for creating CentOS and Fedora images for Rock64 devices.
 
-## Instructions (Manual)
+## Instructions (Automatic Build)
 
-These steps are verified to be work on setting up CentOS 8 on a Rock64. Refer to the non-working rock64_fedora.sh script for how the backup and restore steps are handled.
+Create the Rock64 image for CentOS or downliad is from the [releases page](https://github.com/ekultails/rock64_fedora/releases).
+
+```
+$ sudo bash rock64_fedora.sh
+```
+
+Use `dd` to copy the `/root/.rock64_fedora_tmp/centos-8.0-minimal-rock64-0.2.0.img` image to a microSD card.
+
+## Instructions (Manual Build)
+
+These steps are verified to be work on setting up CentOS 8 on a Rock64.
 
 - `dd` a Debian 10/Buster Rock64 images from the [official releases](https://github.com/ayufan-rock64/linux-build/releases) to a microSD card.
 - Boot the Rock64 up. Scripts will run to automatically resize the root partitions.
@@ -17,8 +27,13 @@ These steps are verified to be work on setting up CentOS 8 on a Rock64. Refer to
 - `rsync` the backed up Rock64 directories back onto the microSD card. `/lib` libraries will need to go into `/usr/lib` on CentOS.
 - Boot the Rock64 and log into the `root` account with the password `rock64`. It will then prompt to change the root password.
 
+## Known Issues
+
+-  DNF does not recognize the `releasever` used in repository URLs. This must be manually set. For example: `dnf update --releasever 8` for CentOS 8.0.
+
 ## Scripts Change Log
 
+- 0.2.0 = Use Debian 10/Buster instead of 9/Stretch. Use a prebuilt root filesystem (container images are no longer used). Use a folder in the `$HOME` directory for temporary files (instead of /tmp to handle operating systems that use tmpfs with limited space). The rock64_fedora.sh script now works as intended.
 - 0.1.0 = Start of a proof-of-concept script (not working) to use a CentOS 8 container image and a Debian 10 Rock64 baremetal image to build a CentOS 8 baremetal image.
 
 ## License
--- rock64_fedora.sh
+++ rock64_fedora.sh
@@ -1,58 +1,59 @@
 #!/bin/bash
 
-container_cli="docker"
-
-if [ -x $(command -v podman) ]; then
-    echo "Using podman."
-    container_cli="podman"
-else
-    echo "Using docker (may require root access)."
-fi
-
-rock64_release_full="0.9.14-1159"
+set -x
+ROCK64_VERSION="0.2.0"
+echo "rock64_fedora version: $ROCK64_VERSION"
+rock64_release_full="0.9.16-1163"
 rock64_release=$(echo $rock64_release_full | cut -d\- -f1)
-rock64_image="stretch-minimal-rock64-${rock64_release_full}-arm64.img"
+rock64_image="buster-minimal-rock64-${rock64_release_full}-arm64.img"
 rock64_url="https://github.com/ayufan-rock64/linux-build/releases/download/${rock64_release}/${rock64_image}.xz"
 
-TMP_DIR=$(mktemp --directory)
+TMP_DIR="$HOME/.rock64_fedora_tmp"
+mkdir $TMP_DIR
 cd $TMP_DIR
 echo "Working directory: $(pwd)"
-echo "Downloading the root file system from an OCI image."
-$container_cli pull centos:8
-$container_cli save centos:8 > centos_8.tar
-tar -x -f centos_8.tar
-mkdir centos_8_rootfs
-tar -x -f ./*/layer.tar -C centos_8_rootfs
+wget https://github.com/ekultails/rock64_fedora/releases/download/0.1.0/centos-8.0-aarch64-rootfs.tar.xz
+tar -x -f centos-8.0-aarch64-rootfs.tar.xz
+# Clean up existing image that may be modified.
+rm -f $rock64_image
 wget $rock64_url
 xz -d ${rock64_image}.xz
-loop_device=$(losetup --show -f -P $rock65_image)
+loop_device=$(losetup --show -f -P $rock64_image)
 mkdir mnt
-# root
+# Mount the root partition.
 mount ${loop_device}p7 ${TMP_DIR}/mnt
-# /boot/, /vendor/, /lib/firmware/, and /lib/modules/ need to be kept
-# from the Rock64 image. Optionally also keep /usr/src/ which has the
-# kernel source code.
-cd centos_8_rootfs
+# Copy over the linux kernel and other required files from the official Rock64 image.
+rock64_fedora_image="centos-8.0-minimal-rock64-$ROCK64_VERSION"
+mkdir $rock64_fedora_image
+cd $rock64_fedora_image
+cp -r ../centos-8.0-aarch64-rootfs/* ./
+chmod 1777 ./tmp
+rm -rf ./var/cache/yum/*
+> ./etc/machine-id
 cp -r ${TMP_DIR}/mnt/boot ./
 cp -r ${TMP_DIR}/mnt/vendor ./
 # Fedora uses a symlink from /lib to /usr/lib.
 cp -r ${TMP_DIR}/mnt/lib/firmware usr/lib/
 cp -r ${TMP_DIR}/mnt/lib/modules usr/lib/
 cp -r ${TMP_DIR}/mnt/usr/src/linux-headers-* usr/src/kernels/
+# Copy the root filesystem resizing script.
+cp ${TMP_DIR}/mnt/etc/systemd/system/first-boot.service etc/systemd/system/first-boot.service
+ln -s /usr/lib/systemd/system/first-boot.service etc/systemd/system/multi-user.target.wants/first-boot.service
+cp ${TMP_DIR}/mnt/usr/local/sbin/rock64_first_boot.sh usr/local/sbin/
+cp ${TMP_DIR}/mnt/usr/local/sbin/resize_rootfs.sh usr/local/sbin/
 # Enable the OpenSSH server.
-ln -s /usr/lib/systemd/system/sshd.service /etc/systemd/system/multi-user.target.wants/sshd.service
+ln -s /usr/lib/systemd/system/sshd.service etc/systemd/system/multi-user.target.wants/sshd.service
 # The root password will fail to reset without this option.
 # https://bugzilla.redhat.com/show_bug.cgi?id=1410450
 echo "dictcheck = 0" >> etc/security/pwquality.conf
-# Testing using `find` to delete files.
-# cd ${TMP_DIR}/mnt
-#find . -type d -not \( -wholename "./boot*" -or -wholename "./lib/firmware*" -or -wholename "./lib/modules*" -or -wholename "./usr/src*" \) -delete
 # Add the boot entry to the fstab (mirroring the configuration from the Rock64 image).
 echo "LABEL=boot /boot/efi vfat defaults,sync 0 0" > etc/fstab
-# Set password the root password to "rock64"
+# Set the initial password for root to "rock64".
 sed -i s'/root.*/root:$6$2Nr3piSK$Ko3rqyUCr2yadIhU0KATBTlEILsq2OdhNzlOv46n2QkiYXAp0WJnzLgO93DX9SxQjgXlAHw5mMApAZkb8ODZf.:0:0:99999:7:::/g' etc/shadow
-rm -f ${TMP_DIR}/mnt/*
-cp -a -r ${TMP_DIR}/centos_8_rootfs/* ${TMP_DIR}/mnt/
-# Limitations
-#     SSH is not installed
-#     Non-privileged user must be manually created
+rm -rf ${TMP_DIR}/mnt/*
+cp -a -r ${TMP_DIR}/${rock64_fedora_image}/* ${TMP_DIR}/mnt/
+sync
+umount ${TMP_DIR}/mnt
+losetup -D $loop_device
+cd ${TMP_DIR}
+mv buster-minimal-rock64-0.9.16-1163-arm64.img ${rock64_fedora_image}.img
